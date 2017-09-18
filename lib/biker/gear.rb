57524d3a226232e500e4c6314a38ccd732fe15a5
Gerard Van Halsema <gerardvh@me.com>
2017-09-17 21:04:43 -0500

    Gear uses Wheel class instead of duplicating functionality.

--- lib/biker/gear.rb
+++ lib/biker/gear.rb
@@ -1,10 +1,12 @@
+require 'wheel'
+
 class Gear
   attr_reader :chainring, :cog, :wheel
 
-  def initialize(chainring, cog, rim, tire)
+  def initialize(chainring, cog, wheel=nil)
     @chainring  = chainring
     @cog        = cog
-    @wheel      = Wheel.new(rim, tire)
+    @wheel      = wheel
   end
 
   def ratio
@@ -13,11 +15,5 @@ class Gear
 
   def gear_inches
     ratio * wheel.diameter
-  end
-
-  Wheel = Struct.new(:rim, :tire) do
-    def diameter
-      rim + (tire * 2)
-    end
   end
 end
--- spec/gear_spec.rb
+++ spec/gear_spec.rb
@@ -2,8 +2,12 @@ require 'spec_helper'
 require 'gear'
 
 RSpec.describe Gear do
-  it 'computes gear inches' do
-    gi = Gear.new(52, 11, 26, 1.5).gear_inches
-    expect(gi).not_to be nil
+  context 'when it has a wheel' do
+    wheel = Wheel.new(26, 1.5)
+
+    it 'computes gear inches' do
+      gi = Gear.new(52, 11, wheel).gear_inches
+      expect(gi).to be_within(0.1).of(137)
+    end
   end
 end

5edfbcae6ef1cde7f5e0f9b48487f666d6d36695
Gerard Van Halsema <gerardvh@me.com>
2017-09-19 09:58:39 -0500

    Page 45 - isolate dependencies on other objects and messages.

--- lib/biker/gear.rb
+++ lib/biker/gear.rb
@@ -17,6 +17,16 @@ class Gear
     # Important here that we are injecting the 'wheel' object instead of manually
     # creating an instance with `Wheel.new(...)`. This way, `Gear` can deal with
     # any thing that responds to the `.diameter` message.
-    ratio * wheel.diameter
+    ratio * diameter
+  end
+
+  #
+  # This is a defensive code-isolation technique. Take *any* references to
+  # outside objects or messages to anything other than `self` and isolate them
+  # so as to reduce the possibility of creeping dependency on that object.
+  #
+  # @return [int] Diameter of the `wheel` passed in the `initialize` method.
+  def diameter
+    wheel.diameter
   end
 end
